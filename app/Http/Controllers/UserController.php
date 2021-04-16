0286828ed7f5bdf86a8afa7239aee2c8c7a7c2dd
wendelulhoa <wendelulhoa@gmail.com>
2021-04-16 10:53:51 -0300

    first-commit

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Exception;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    public function index(){
+
+    }
+
+    public function create(Request $request){
+        DB::beginTransaction();
+        try{
+            User::create([
+                'name'      => $request['username'],
+                'email'     => $request['email'],
+                'password'  => Hash::make($request['password']),
+                'type_user' => 0
+            ]);
+        DB::commit();
+        }catch(Exception $e){
+        DB::rollback();
+            return redirect()->route('view-create');
+        }
+    }
+}
