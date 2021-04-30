03de3ac885e84e69f9edbde1ddaab0f7cda02401
wendelulhoa <wendelulhoa@gmail.com>
2021-04-30 13:35:10 -0300

    colocado o link para ir para os detalhes

--- resources/views/mods/card-mods.blade.php
+++ resources/views/mods/card-mods.blade.php
@@ -20,6 +20,10 @@
     .borda-arredondada {
         border-radius: 20px;
     }
+    .product-info{
+        left: 13% !important;
+        bottom: 50%;
+    }
 </style>
 @foreach ($mods as $key => $item)
 @php
@@ -30,31 +34,32 @@
         <div class="card-body">
             <div class="product">
                 <div class="text-center product-img">
+                <a href="{{ Route('mods-detail',['id'=>$item->id]) }}">
                     <img src="{{ Route('index') . '/'. $image[0]->path }}" alt="img" class="img-fluid img-responsive">
+                </a>
                 </div>
-                <div class=" text-center mt-4">
+                <div class=" text-center mt-4" style="text-overflow: ellipsis;">
                     <div class="text-center text-warning">
                         <i class="fa fa-star"></i>
                         <i class="fa fa-star"></i>
-                        <i class="fa fa-star-half-o"></i>
-                        <i class="fa fa-star-o"></i>
-                        <i class="fa fa-star-o"></i>
+                        <i class="fa fa-star"></i>
                     </div>
-                    <a href="#"><h5 class="mb-0 mt-2">{{$item->name}}</h5></a>
-                    <div class="price mt-3 h4 mb-0">
-                        <s class="h4 text-muted mr-4">$80.00</s>$59.00
+                    <div class="row pt-3 ml-auto" style="padding-left: 50px">
+                        <i class="fas fa-download fa-1x text-success"></i>
+                        <p class="pl-2 pr-2">100</p>
+                        <i class="fas fa-star text-warning"></i>
+                        <p class="pl-2 pr-2">5</p>
+                        <i class="fas fa-thumbs-up text-info"></i>
+                        <p class="pl-2">10</p>
                     </div>
+                    <a href="{{ Route('mods-detail',['id'=>$item->id]) }}"><h5 class="mb-0 mt-2">{{$item->name}}</h5></a>
                 </div>
                 <div class="product-info">
-                    <a href="#" class="btn  btn-info btn-sm mt-1 mb-1 text-sm  text-white">
-                        <i class="fe fe-heart"></i>
-                    </a>
-                    <a href="#" class="btn btn-icon btn-sm btn-danger mt-1 text-sm  mb-1 text-white">
-                        <i class="fe fe-share-2"></i>
-                    </a>
-                    <a href="#" class="btn btn-icon btn-sm btn-warning mt-1 text-sm  mb-1 text-white">
-                        <i class="fe fe-shopping-cart"></i>
-                    </a>
+                    <div class="d-flex ">
+                        <a href="" class="badge badge-default mr-1"><i class="fas fa-tag"></i> CARRO</a>
+                        <a href="" class="badge badge-default mr-1"><i class="fas fa-tag"></i> CARRO</a>
+                        <a class="badge badge-info"> VERSÃO 1.01</a>
+                    </div>
                 </div>
             </div>
         </div>
--- resources/views/mods/mods.blade.php
+++ resources/views/mods/mods.blade.php
@@ -3,8 +3,6 @@
 @section('content')
     <div class="row m-2 mr-5 pr-5">
         @include('mods.card-mods', compact('mods'))
-        @include('mods.card-mods', compact('mods'))
-        @include('mods.card-mods', compact('mods'))
     </div>
     
 @endsection
