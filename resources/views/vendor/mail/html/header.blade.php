e770b09a2a70cce843cda7243ee9a2908af8d437
wendelulhoa <wendelulhoa@gmail.com>
2021-05-25 14:40:51 -0300

    email

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,7 +2,7 @@
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
 @if (trim($slot) === 'Ulhoa-mods')
-<img src="{{ Route('index') }}images/logo.png" class="logo" alt="{{ config('app.name') }}">
+<img src="{{ Route('index') }}/images/logo.png" class="logo" alt="{{ config('app.name') }}">
 @else
 {{ $slot }}
 @endif
