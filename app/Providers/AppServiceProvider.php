514e68a4dc544eafd54d20a800c6777b65b1ad99
wendelulhoa <wendelulhoa@gmail.com>
2021-05-30 00:34:06 -0300

    melhorias

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -28,6 +28,9 @@ class AppServiceProvider extends ServiceProvider
         Schema::defaultStringLength(191);
         if($this->app->environment('production')) {
             URL::forceScheme('https');
+            if(strripos(Route('index'), '157.230.226.98')){
+                return redirect('https://www.ulhoamods.com');
+            }
         }
     }
 }
